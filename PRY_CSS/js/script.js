cb61ad95f68be5838ca31a68b2f7595e68165747
Alexander Jhosue Toapanta Jama <141873111+AlexanderToapanta@users.noreply.github.com>
2025-05-13 17:02:58 -0500

    mi parte

--- PRY_CSS/js/script.js
+++ PRY_CSS/js/script.js
@@ -1,32 +1,71 @@
+let nombre_r ;
+let apellido_r ;
+let correo_r  ;
+let password_r ;
+
 function Login() {
     let emailInput = document.getElementById('txt_email_login');
     let passwordInput = document.getElementById('txt_password_login');
     let email = emailInput.value;
     let password = passwordInput.value;
 
-    if (email === "alexandertopanata05@gmail.com" && password === "12346") {
-    alert('Inicio de sesión correcto');
-    cargarPaginas('dashboard');
-} else if (email === "ajtoapanta6@espe.edu.ec" && password === "12345") {
-    alert('Inicio de sesión correcto');
-    cargarPaginas('dashboard');
-} else {
-    alert('Correo o contraseña erróneos');
-    emailInput.value = '';
-    passwordInput.value = '';
-    emailInput.focus();
-}
+    let storedEmail = localStorage.getItem('correo_r');
+    let storedPassword = localStorage.getItem('password_r');
+    let storedNombre = localStorage.getItem('nombre_r');
+    let storedApellido = localStorage.getItem('apellido_r');
+
+    if (email === "alexandertoapanta05@gmail.com" && password === "12346") {
+        alert('Inicio de sesión correcto');
+        cargarPaginas('dashboard');
+        document.getElementById('navbarDropdown').style.display = 'inline-block';
+        document.getElementById('email_usuer').textContent = "alexandertoapanta05@gmail.com";
+
+    } else if (email === storedEmail && password === storedPassword) {
+        alert('Inicio de sesión correcto');
+        cargarPaginas('dashboard');
+        document.getElementById('navbarDropdown').style.display = 'inline-block';
+        document.getElementById('prub').style.display = 'none';
+        document.getElementById('movimientos').style.display = 'none';
+        document.getElementById('nombre_usuario').textContent = "Nombre: " + storedNombre;
+        document.getElementById('apellido_usuario').textContent = "Apellido: " + storedApellido;
+        document.getElementById('correo_usuario').textContent = "Correo: " + storedEmail;
+        document.getElementById('email_usuer').textContent = storedEmail;
+    } else {
+        alert('Correo o contraseña erróneos');
+        emailInput.value = '';
+        passwordInput.value = '';
+        emailInput.focus();
+    }
 }
 
+
 function Registro(){
+let emailInput = document.getElementById('txt_email_Registro');
+let passwordInput = document.getElementById('txt_password_Registro');
+let nombreInput = document.getElementById('txt_Nombre_Registro');
+let apellidoInput = document.getElementById('txt_apellido_Registro');
+let email = emailInput.value;
+let nombre = nombreInput.value;
+let apellido = apellidoInput.value;
+let password = passwordInput.value;
+ 
+if(email === "alexandertoapanta05@gmail.com"){
+alert('Correo ya ingresado');
+}else{   
+localStorage.setItem('nombre_r', nombre);
+localStorage.setItem('apellido_r', apellido);
+localStorage.setItem('correo_r', email);
+localStorage.setItem('password_r', password);
+alert('Resgitro Completado');
+cargarPaginas('index');
+
+}
 
 }
 
 function CerrarSesion(){
-    document.getElementById('btn_usuario').style.display='none';
-    document.getElementById('btn_Login').style.display='inline-block';
-    document.getElementById('btn_admin').style.display = 'none';
-    document.getElementById('btn_Stock').style.display = 'none';
+    document.getElementById('navbarDropdown').style.display = 'none';
+    document.getElementById('email_usuer').textContent = " ";
     alert('Sesion cerrada correctamente');
-    cargarPaginas('index');
+    cargarPaginas('bienvenida');
 }
