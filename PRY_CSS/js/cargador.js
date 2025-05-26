00e64552735aeb03655d2fabe1e42e5b49816bec
Alexander Jhosue Toapanta Jama <141873111+AlexanderToapanta@users.noreply.github.com>
2025-05-26 09:28:13 -0500

    Form

--- PRY_CSS/js/cargador.js
+++ PRY_CSS/js/cargador.js
@@ -1,3 +1,4 @@
+
 fetch("./paginas/header.html")
   .then(res => res.text())
   .then(data => document.getElementById("menu").innerHTML = data);
@@ -28,29 +29,39 @@ function cargarPaginas(url_pagina) {
         };
         document.body.appendChild(script);
       } else if (url_pagina === 'signup') {
+        
         if (typeof Login === 'function') {
           Login();
         }
       }
       if (url_pagina === "usuario") {
         cargarUsuario();
+        cargarDatosAdmin(cambio, nombre, apellido);
+       
+      }
+       if (url_pagina === "actualizacion_datos") {
+       CargarDatosUsuario();
+       
+       
       }
     });
 }
 
 function cargarUsuario() {
+ let correo = document.getElementById('email_usuer').textContent;
+   let Nombre = localStorage.getItem('nombre_r');
+    let Apellido = localStorage.getItem('apellido_r');
 
-  let correo = document.getElementById('email_usuer').textContent;
-   let storedNombre = localStorage.getItem('nombre_r');
-    let storedApellido = localStorage.getItem('apellido_r');
+ 
   document.getElementById('correo_usuario').textContent = "Correo: " + correo;
-  document.getElementById('nombre_usuario').textContent = "Nombre: " + storedNombre + " " + storedApellido;
+  document.getElementById('nombre_usuario').textContent = "Nombre: " + Nombre + " " + Apellido;
  document.getElementById('movimientos').style.display = 'none';
 document.getElementById('movimientos_s').style.display = 'inline-block';
 document.getElementById('img_usuarios').style.display = 'inline-block';
 document.getElementById('img_usuario').style.display = 'none';
 
-if(correo==="alexandertoapanta05@gmail.com"){
+
+if(correo=== "alexandertoapanta05@gmail.com"){
   document.getElementById('correo_usuario').textContent = "Correo: " + "alexandertoapanta05@gmail.com";
   document.getElementById('nombre_usuario').textContent = "Nombre: " + "Alexander" + " " + "Toapanta";
  document.getElementById('movimientos').style.display = 'inline-block';
@@ -58,8 +69,73 @@ document.getElementById('movimientos_s').style.display = 'none';
 document.getElementById('img_usuarios').style.display = 'none';
 document.getElementById('img_usuario').style.display = 'inline-block';
 
-}
-  
-  
+} 
 
 }
+
+/* function cargarDatosAdmin(cambio, nombre, apellido){
+if(cambio=true){
+document.getElementById('correo_usuario').textContent = "Correo: " + "alexandertoapanta05@gmail.com";
+document.getElementById('nombre_usuario').textContent = "Nombre: " + nombre + " " + apellido;
+}
+} */
+
+function CargarDatosUsuario() {
+  let correo = document.getElementById('email_usuer').textContent;
+  let nombre = localStorage.getItem('nombre_r');
+  let apellido = localStorage.getItem('apellido_r');
+  let password = localStorage.getItem('password_r');
+  let codigoPais = localStorage.getItem('codigopais_r');
+  let telefono = localStorage.getItem('num_celular_r');
+  let genero = localStorage.getItem('genero_r');
+  let genero_t="Hombre"
+  let fecha = localStorage.getItem('fecha_nacimiento_r');
+  let musica = JSON.parse(localStorage.getItem('musica_fav_r') );
+
+  document.getElementById('txt_correo_actualizado').value = correo;
+  document.getElementById('txt_nombre_actualizado').value = nombre ;
+  document.getElementById('txt_apellido_actualizado').value = apellido ;
+  document.getElementById('password_actualizada').value = password ;
+
+  document.getElementById('codigo_pais_actualizado').value = codigoPais ;
+  document.getElementById('telefono_actualizado').value = telefono ;
+  document.getElementById('fecha_actualizada').value = fecha ;
+
+  
+  if (genero) {
+    let generoRadio = document.querySelector(`input[name="genero_actualizado"][value="${genero}"]`);
+    if (generoRadio) generoRadio.checked = true;
+  }
+
+
+  document.querySelectorAll('input[name="musica_actualizada"]').forEach((checkbox) => {
+    checkbox.checked = musica.includes(checkbox.value);
+  });
+
+
+  if (correo === "alexandertoapanta05@gmail.com") {
+    document.getElementById('txt_correo_actualizado').readOnly = true;
+    document.getElementById('password_actualizada').readOnly = true;
+    document.getElementById('txt_correo_actualizado').value = "alexandertoapanta05@gmail.com";
+    document.getElementById('txt_nombre_actualizado').value = "Alexander";
+    document.getElementById('txt_apellido_actualizado').value = "Toapanta";
+    document.getElementById('password_actualizada').value = "12345";
+  document.getElementById('codigo_pais_actualizado').value =  "+593";
+  document.getElementById('telefono_actualizado').value = "983845189" ;
+  document.getElementById('fecha_actualizada').value = "2004-11-01" ;
+
+  
+  if (genero) {
+    let generoRadio = document.querySelector(`input[name="genero_actualizado"][value="${genero_t}"]`);
+    if (generoRadio) generoRadio.checked = true;
+  }
+
+
+  document.querySelectorAll('input[name="musica_actualizada"]').forEach((checkbox) => {
+    checkbox.checked = (checkbox.value === "Silverbullet");
+});
+
+  }
+}
+
+
--- PRY_CSS/js/script.js
+++ PRY_CSS/js/script.js
@@ -2,20 +2,30 @@ let *nombre_r ;
 let *apellido_r ;
 let *correo_r  ;
 let *password_r ;
+let *num_celular_r;
+let *genero_r;
+let *codigopais_r;
+let *musica_fav_r;    
+let* fecha_nacimiento_r;
+
+let *nombre_p;
+let *apellido_p ;
+let *correo_p ;
+let *password_p ;
+
+let actualizacion;
 
 function Login() {
     let emailInput = document.getElementById('txt_email_login');
     let passwordInput = document.getElementById('txt_password_login');
     let email = emailInput.value;
     let password = passwordInput.value;
-    let correo= document.getElementById('email_usuer').value;
-
     let storedEmail = localStorage.getItem('correo_r');
     let storedPassword = localStorage.getItem('password_r');
     let storedNombre = localStorage.getItem('nombre_r');
     let storedApellido = localStorage.getItem('apellido_r');
 
-    if (email === "alexandertoapanta05@gmail.com" && password === "12346") {
+    if (email === "alexandertoapanta05@gmail.com" && password === "12345") {
         alert('Inicio de sesión correcto');
         cargarPaginas('dashboard');
         document.getElementById('navbarDropdown').style.display = 'inline-block';
@@ -35,33 +45,94 @@ function Login() {
 }
 
 
-function Registro(){
-let emailInput = document.getElementById('txt_email_Registro');
-let passwordInput = document.getElementById('txt_password_Registro');
-let nombreInput = document.getElementById('txt_Nombre_Registro');
-let apellidoInput = document.getElementById('txt_apellido_Registro');
-let email = emailInput.value;
-let nombre = nombreInput.value;
-let apellido = apellidoInput.value;
-let password = passwordInput.value;
-if(email === "alexandertoapanta05@gmail.com"){
-alert('Correo ya ingresado');
-}else{   
-localStorage.setItem('nombre_r', nombre);
-localStorage.setItem('apellido_r', apellido);
-localStorage.setItem('correo_r', email);
-localStorage.setItem('password_r', password);
-alert('Resgitro Completado');
-cargarPaginas('index');
+
+function Registro() {
+    let email = document.getElementById('txt_email_Registro').value.trim();
+    let password = document.getElementById('txt_password_Registro').value.trim();
+    let nombre = document.getElementById('txt_Nombre_Registro').value.trim();
+    let apellido = document.getElementById('txt_apellido_Registro').value.trim();
+    let codigoPais = document.getElementById('codigo_pais').value;
+    let telefono = document.getElementById('telefono').value.trim();
+    let genero = document.querySelector('input[name="genero"]:checked');
+    let fecha = document.getElementById('fecha').value;
+
+    let musicaSeleccionada = [];
+    document.querySelectorAll('input[name="musica[]"]:checked').forEach((checkbox) => {
+        musicaSeleccionada.push(checkbox.value);
+    });
+
+    if (!email || !nombre || !apellido || !password || !telefono || !genero || !fecha) {
+        alert('Por favor, completa todos los campos requeridos.');
+        return;
+    }
 
 
+    if (email === "alexandertoapanta05@gmail.com") {
+        alert('Correo ya ingresado');
+        
+    }else{
+    localStorage.setItem('nombre_r', nombre);
+    localStorage.setItem('apellido_r', apellido);
+    localStorage.setItem('correo_r', email);
+    localStorage.setItem('password_r', password);
+    localStorage.setItem('codigopais_r', codigoPais);
+    localStorage.setItem('num_celular_r', telefono);
+    localStorage.setItem('genero_r', genero.value);
+    localStorage.setItem('fecha_nacimiento_r', fecha);
+    localStorage.setItem('musica_fav_r', JSON.stringify(musicaSeleccionada));
+    alert('Registro completado')
+    cargarPaginas('index');
+    
+    }
+
+    
 }
 
-}
+
+   
+
 
 function CerrarSesion(){
     document.getElementById('navbarDropdown').style.display = 'none';
     document.getElementById('email_usuer').textContent = " ";
     alert('Sesion cerrada correctamente');
     cargarPaginas('bienvenida');
+}
+
+function Actualizar(){
+let emailInput = document.getElementById('txt_correo_actualizado');
+let nombreInput = document.getElementById('txt_nombre_actualizado');
+let apellidoInput = document.getElementById('txt_apellido_actualizado');
+let passwordInput = document.getElementById('password_actualizada');
+let codigoPais = document.getElementById('codigo_pais_actualizado').value;
+let telefono = document.getElementById('telefono_actualizado').value.trim();
+let genero = document.querySelector('input[name="genero_actualizado"]:checked');
+let fecha = document.getElementById('fecha_actualizada').value;
+let email = emailInput.value;
+let nombre = nombreInput.value;
+let apellido = apellidoInput.value;
+let password = passwordInput.value;
+
+ if (!email || !nombre || !apellido || !password || !telefono || !genero || !fecha) {
+        alert('Por favor, completa todos los campos requeridos.');
+        return;
+    }
+
+if(email === "alexandertoapanta05@gmail.com"){
+alert('Cambios correctamente realizados')
+ return;
+ 
+} 
+alert('Datos Actualizados')
+localStorage.setItem('nombre_r', nombre);
+localStorage.setItem('apellido_r', apellido);
+localStorage.setItem('correo_r', email);
+ document.getElementById('email_usuer').textContent = email;
+localStorage.setItem('password_r', password);
+localStorage.setItem('codigopais_r', codigoPais);
+localStorage.setItem('num_celular_r', telefono);
+localStorage.setItem('genero_r', genero.value);
+localStorage.setItem('fecha_nacimiento_r', fecha);
+localStorage.setItem('musica_fav_r', JSON.stringify(musicaSeleccionada));
+
 }
